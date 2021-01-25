974f416bbb4f2209d695d9f22163079ae6deed81
Jonathan Rod <jonyrm91@gmail.com>
2021-01-25 14:58:11 +0100

    Image converter done(path.join)

--- Nodepop/microservices/imageConverter.js
+++ Nodepop/microservices/imageConverter.js
@@ -1,6 +1,7 @@
 'use strict';
 const cote = require('cote');
 const jimp = require('jimp');
+const path = require('path');
 
 /**
  * Image resizing service
@@ -14,11 +15,11 @@ responder.on('convert', async (req, done) => {
   console.log(`Creating thumbnail...${req.name}`, req.name, Date.now());
 
   // We obtain the result
-  const fileName = '../../public/images' + req.name;
+  const fileName = path.join(__dirname, '../public/images/' + req.name)
   console.log(fileName);
   const image = await jimp.read(fileName);
   console.log(image);
-  const result = await image.resize(100, 100).write(fileName);
+  const result = await image.resize(100, 100).write(path.join(__dirname, '../public/images/thumbnails/' + req.name));
   done(result);
 });
 
